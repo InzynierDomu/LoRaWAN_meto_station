c976132ec33e711a9c98eb266e9435b7c6714c4c
Inżynier Domu <inzynier.domu@gmail.com>
2022-12-27 19:01:40 +0100

    add repo new standard files

--- LoRaWAN_meto_station/src/main.cpp
+++ LoRaWAN_meto_station/src/main.cpp
@@ -6,32 +6,31 @@
  * @date 11-2019
  */
 
-#include <Arduino.h>
+#include "ArduinoLowPower.h"
 #include "Config.h"
-#include "Pin_config.h"
-#include "Results.h"
 #include "LoRaWAN.h"
 #include "Measurements.h"
-#include "ArduinoLowPower.h"
+#include "Pin_config.h"
+#include "Results.h"
+#include <Arduino.h>
+
 
 Measurements *m_measurements;
 LoRaWAN *m_lorawan;
 Results m_results;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  delay(60000);
+  delay(60000); // TODO: test without this
   m_measurements = new Measurements();
   m_lorawan = new LoRaWAN();
 
-  m_measurements->Init_sensors();
   m_lorawan->Setup();
+  m_measurements->Init_sensors();
 }
 
-void loop()
-{
+void loop() {
   m_measurements->Measure(&m_results);
   m_lorawan->Send_msg_measurements(&m_results);
-	LowPower.deepSleep(Config::uplink_interval);
+  LowPower.deepSleep(Config::uplink_interval);
 }
