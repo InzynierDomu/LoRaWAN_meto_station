c3cd40b9616719d7c4222e845d14519ec089ab3d
Inżynier Domu <inzynier.domu@gmail.com>
2025-05-08 15:10:32 +0200

    add new parameter to decoder

--- ttn_decoder.js
+++ ttn_decoder.js
@@ -1,46 +1,58 @@
 function Decoder(bytes, port) {
-  var decoded = {};
-
-  var msg = String.fromCharCode.apply(null, bytes);
-	var i = 1; 
-	var temp = '';
-	var hum = '';
-	var pres = '';
-	var light = '';
+	var decoded = {};
+  
+	var msg = String.fromCharCode.apply(null, bytes);
+	  var i = 1; 
+	  var temp = '';
+	  var hum = '';
+	  var pres = '';
+	  var light = '';
+	  var moisture ='';
+	  
+	  while(i < msg.length && msg[i] != "H")
+	  {
+		temp += msg[i];
+		i++;
+	  }
+	decoded.temp = temp;
 	
-	while(i < msg.length && msg[i] != "H")
-	{
-	  temp += msg[i];
-	  i++;
-	}
-  decoded.temp = temp;
-  
-  i++;
-  
-  while(i < msg.length && msg[i] != "P")
-	{
-	  hum += msg[i];
-	  i++;
-	}
-  decoded.hum = hum;
-  
-  i++;
-  
-  while(i < msg.length && msg[i] != "L")
-	{
-	  pres += msg[i];
-	  i++;
-	}
-  decoded.pres = pres;
-  
-  i++;
-  
-  while(i < msg.length)
-	{
-	  light += msg[i];
-	  i++;
-	}
-  decoded.light = light;
-  
-  return decoded;
-}
+	i++;
+	
+	while(i < msg.length && msg[i] != "P")
+	  {
+		hum += msg[i];
+		i++;
+	  }
+	decoded.hum = hum;
+	
+	i++;
+	
+	while(i < msg.length && msg[i] != "L")
+	  {
+		pres += msg[i];
+		i++;
+	  }
+	decoded.pres = pres;
+	
+	i++;
+	
+	while(i < msg.length && msg[i] != "M")
+	  {
+		light += msg[i];
+		i++;
+	  }
+	decoded.light = light;
+	
+	i++;
+	
+	while(i < msg.length)
+	  {
+		moisture += msg[i];
+		i++;
+	  }
+	decoded.moisture = moisture;
+	
+	i++;
+	
+	return decoded;
+  }
